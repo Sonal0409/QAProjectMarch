b232b02db3b6c0efe1c0cd4ff9e95b2cef32923e
Sonal <mittal.sonal04@gmail.com>
2021-03-21 14:06:48 -0400

    new file

---
+++ src/test/java/com/qa/testcases/TestCase2.java
@@ -0,0 +1,13 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+
+	
+	@Test
+	public void TestMethod1()
+	{
+		System.out.println("selenium test case 2");
+	}
+}
